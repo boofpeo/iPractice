3518e570ec496da626c041371f2cdd1a800bc872
zhang <kes6863haobao@163.com>
2019-12-10 19:34:44 +0800

    第三次提交

--- repidStart/src/main/java/com/zhang/repidStart/RepidStartApplication.java
+++ repidStart/src/main/java/com/zhang/repidStart/RepidStartApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 项目专用
+ * 项目专用测试
  */
 @SpringBootApplication
 public class RepidStartApplication {
