8d9b9c23786aaac2e781928a991b6c6b6fc3dd21
zhang <kes6863haobao@163.com>
2019-12-10 19:08:36 +0800

    IDEA 提交

--- repidStart/src/main/java/com/zhang/repidStart/RepidStartApplication.java
+++ repidStart/src/main/java/com/zhang/repidStart/RepidStartApplication.java
@@ -3,6 +3,9 @@ package com.zhang.repidStart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 项目专用
+ */
 @SpringBootApplication
 public class RepidStartApplication {
 
