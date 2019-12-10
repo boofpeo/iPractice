ac6f2e3677cf84c87bed8f041e51a7f74e82ea00
unknown <yu.zhang@xquant.com>
2019-12-10 18:51:20 +0800

    ideaCommit

---
+++ repidStart/src/main/java/com/zhang/repidStart/RepidStartApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.repidStart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepidStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepidStartApplication.class, args);
+	}
+
+}
---
+++ repidStart/src/test/java/com/zhang/repidStart/RepidStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhang.repidStart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepidStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
