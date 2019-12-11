7982154903f9e8ea1f20ecf37b7d10bc843b1b0f
zhang <kes6863haobao@163.com>
2019-12-11 10:29:09 +0800

    测试第一次学习使用

--- learn/src/main/java/com/zhang/learn/LearnApplication.java
+++ learn/src/main/java/com/zhang/learn/LearnApplication.java
@@ -3,6 +3,9 @@ package com.zhang.learn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 学习使用
+ */
 @SpringBootApplication
 public class LearnApplication {
 
