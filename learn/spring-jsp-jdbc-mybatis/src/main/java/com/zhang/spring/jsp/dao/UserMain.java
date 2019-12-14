af9be9b4fe93aea76956986414ec483c7c581222
Howio <kes6863haobao@163.com>
2019-12-14 15:37:48 +0800

    mybatis 例子

---
+++ learn/spring-jsp-jdbc-mybatis/src/main/java/com/zhang/spring/jsp/dao/UserMain.java
@@ -0,0 +1,42 @@
+package com.zhang.spring.jsp.dao;
+
+import com.zhang.spring.jsp.mapping.UserEntity;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.EncodedResource;
+
+import java.io.Reader;
+import java.util.List;
+import java.util.Properties;
+
+public class UserMain {
+
+    public static void main(String[] args) throws Exception {
+
+        ResourceLoader loader = new DefaultResourceLoader();
+
+        Resource resource = loader.getResource("classpath:/mybatis/mybatis-config.xml");
+
+        EncodedResource encodedResource = new EncodedResource(resource, "UTF-8");
+
+        Reader reader = encodedResource.getReader();
+
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+
+        SqlSessionFactory build = sqlSessionFactoryBuilder.build(reader, "macDev", new Properties());
+
+        SqlSession sqlSession = build.openSession();
+
+        List<UserEntity> users = sqlSession.selectList("com.zhang.spring.jsp.dao.UserMapper.selectAll");
+
+        for (UserEntity entity : users) {
+            System.out.println(entity);
+        }
+
+
+    }
+}
---
+++ learn/spring-jsp-jdbc-mybatis/src/main/java/com/zhang/spring/jsp/mapping/UserEntity.java
@@ -0,0 +1,41 @@
+package com.zhang.spring.jsp.mapping;
+
+public class UserEntity {
+
+    private int id;
+    private String name;
+    private String age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
