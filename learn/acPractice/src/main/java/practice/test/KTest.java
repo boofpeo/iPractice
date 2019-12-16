4241580ebc4dfec3bbc5126acda3805d6531b673
zhang <kes6863haobao@163.com>
2019-12-16 19:20:33 +0800

    2019-12-16/19:19 测试和一些一致性理论

---
+++ learn/acPractice/src/main/java/practice/test/KTest.java
@@ -0,0 +1,17 @@
+package practice.test;
+
+public class KTest {
+
+    public static void main(String[] args) {
+        String ms = null;
+        String ko = "";
+//        Integer intMs = Integer.valueOf(ms);
+        Integer intKo = Integer.valueOf(ko);
+//        if (intMs.intValue() == 0) {
+//            System.out.println("yes");
+//        }
+        if (intKo.intValue() == 0) {
+            System.out.println("yes");
+        }
+    }
+}
